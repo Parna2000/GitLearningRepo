91dda1cd7e38f96068d7cc7384a972badc023b13
ParnaGoswami <21MS0093@mc.iitism.ac.in>
2021-09-16 16:05:13 +0530

    array sorted

---
+++ two.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter the length of the array:\n");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter the elements of the array:\n");
+    for (int i = 0; i < n; i++)
+    {
+        /* code */
+        scanf("%d",&arr[i]);
+    }
+    int p=0;
+    printf("Now we have to check whether the above array is sorted or not\n");
+    for (int j = 0; j < n; j++)
+    {
+        /* code */
+        if (arr[j]<arr[j+1]||arr[j]==arr[j+1])
+        {
+            p++;
+            /* code */
+            
+            // continue;
+        }
+        else
+        {
+            /* code */
+            printf("The array is not sorted!!");
+            break;
+        }
+    }
+    if (p==n-1)
+    {
+        /* code */
+        printf("The arraay is sorted");
+    }
+    
+}
